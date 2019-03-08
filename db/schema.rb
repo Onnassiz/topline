597ff73f3850409469c78236d0e700108d55b335
Ben Onah <benjamin.onah@andela.com>
2019-03-08 10:20:23 +0100

    Add Shirt Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_08_083815) do
+ActiveRecord::Schema.define(version: 2019_03_08_090048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,10 +28,21 @@ ActiveRecord::Schema.define(version: 2019_03_08_083815) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "shirts", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
+    t.boolean "in_stock"
+    t.bigint "brand_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["brand_id"], name: "index_shirts_on_brand_id"
+  end
+
   create_table "sizes", force: :cascade do |t|
     t.string "size"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "shirts", "brands"
 end
